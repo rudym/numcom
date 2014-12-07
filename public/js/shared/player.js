37eb3e52adcc33fdf99d879c6583e6ba5f0df9a6
Vladimir Ignatev <ya.na.pochte@gmail.com>
2014-12-07 17:37:55 +0000

    player

--- public/js/shared/player.js
+++ public/js/shared/player.js
@@ -7,63 +7,27 @@ if (typeof define !== 'function') {
 }
 
 define(function(require){ // require is unused
-    /**************************************************
+/**************************************************
 ** GAME PLAYER CLASS
 **************************************************/
-    function Player(startX, startY) {
-        var x = startX,
-        	y = startY,
-        	score = 0,
-        	gems = [],
-        	id;
-        
-        // Getters and setters
-        var getX = function() {
-        	return x;
+    function Player(tile) {
+        this.id = undefined;
+        this.tile = tile;
+        this.score = 0;
+        this.gems = [];
+
+        this.addScore = function(newScore) {
+        	this.score += newScore;
         };
-        
-        var getY = function() {
-        	return y;
-        };
-        
-        var setX = function(newX) {
-        	x = newX;
-        };
-        
-        var setY = function(newY) {
-        	y = newY;
-        };
-        
-        var getScore = function() {
-        	return score;
-        };
-        
-        var addScore = function(newScore) {
-        	score += newScore;
-        };
-        
-        var getGems = function() {
-            return gems;    
-        }
-        
-        var addGem = function(gem) {
+
+        this.addGem = function(gem) {
             gems.push(gem);
         }
-        
-        
-        // Define which variables and methods can be accessed
-        return {
-        	getX: getX,
-        	getY: getY,
-        	setX: setX,
-        	setY: setY,
-        	getScore: getScore,
-        	addScore: addScore,
-        	getGems: getGems,
-        	addGem: addGem,
-        	id: id
-        };
     }
+    
+    
+    // p = new Plyaer();
+    // p.getScore, p.id, 
     
     return {
         'Player': Player
